08c936517861599eb02a2b737cb57b4083aa6e24
Alex Suraci <asuraci@pivotallabs.com>
2013-10-31 18:51:30 -0700

    tabasco: install libcurl and libpq
    needed for creating releases (bundle package + native exts)
    
    Signed-off-by: Phan Le <ple@pivotallabs.com>

--- recipes/packages.rb
+++ recipes/packages.rb
@@ -7,3 +7,10 @@ package 'libxml2-dev'
 
 # for yetis
 package 'maven'
+
+# various libs for bundle package during release creation
+# dea_next:
+package 'libcurl4-openssl-dev'
+
+# cloud_controller_ng:
+package 'libpq-dev'
