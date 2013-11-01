b20516404ef920eaf1b116c7f6a16542f10320eb
Alex Suraci <asuraci@pivotallabs.com>
2013-10-31 18:51:07 -0700

    tabasco: mount ebs volume at /var/lib/jenkins
    Signed-off-by: Phan Le <ple@pivotallabs.com>

--- recipes/aws.rb
+++ recipes/aws.rb
@@ -10,5 +10,21 @@ aws_ebs_volume "db_ebs_volume" do
   aws_secret_access_key node['aws']['secret_access_key']
   volume_id node['aws']['ebs_volume_id']
   device "/dev/sdi"
-  action [ :attach ]
+  action :attach
+end
+
+execute "mkfs -t ext4 /dev/xvdi" do
+  not_if "file -s /dev/xvdi | grep ext4"
+end
+
+directory "/var/lib/jenkins" do
+  owner "root"
+  group "root"
+  mode 00644
+  action :create
+end
+
+mount "/var/lib/jenkins" do
+  device "/dev/xvdi"
+  fstype "ext4"
 end
