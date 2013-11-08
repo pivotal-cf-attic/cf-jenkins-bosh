86045ab4ade91b72b0e814fd639c0032dcc9acaf
Aram Price <aprice@pivotallabs.com>
2013-11-08 09:53:27 -0800

    aws recipe only runs if aws credentials are set
    Signed-off-by: Alex Suraci <asuraci@pivotallabs.com>

--- recipes/aws.rb
+++ recipes/aws.rb
@@ -1,31 +1,33 @@
-aws_elastic_ip 'jenkins ip' do
-  aws_access_key node['aws']['access_key']
-  aws_secret_access_key node['aws']['secret_access_key']
-  ip node['aws']['elastic_ip']
-  action :associate
-end
+if node['aws']['access_key'] && node['aws']['secret_access_key']
+  aws_elastic_ip 'jenkins ip' do
+    aws_access_key node['aws']['access_key']
+    aws_secret_access_key node['aws']['secret_access_key']
+    ip node['aws']['elastic_ip']
+    action :associate
+  end
 
-aws_ebs_volume 'jenkins_ebs_home' do
-  aws_access_key node['aws']['access_key']
-  aws_secret_access_key node['aws']['secret_access_key']
-  volume_id node['aws']['ebs_volume_id']
-  device '/dev/sdi'
-  action :attach
-end
+  aws_ebs_volume 'jenkins_ebs_home' do
+    aws_access_key node['aws']['access_key']
+    aws_secret_access_key node['aws']['secret_access_key']
+    volume_id node['aws']['ebs_volume_id']
+    device '/dev/sdi'
+    action :attach
+  end
 
-execute 'format drive' do
-  command 'mkfs -t ext4 /dev/xvdi'
-  not_if 'file -s /dev/xvdi | grep ext4'
-end
+  execute 'format drive' do
+    command 'mkfs -t ext4 /dev/xvdi'
+    not_if 'file -s /dev/xvdi | grep ext4'
+  end
 
-directory node[:jenkins][:server][:home] do
-  owner 'root'
-  group 'root'
-  mode 00644
-end
+  directory node[:jenkins][:server][:home] do
+    owner 'root'
+    group 'root'
+    mode 00644
+  end
 
-mount node[:jenkins][:server][:home] do
-  device '/dev/xvdi'
-  fstype 'ext4'
-  action [:mount, :enable]
+  mount node[:jenkins][:server][:home] do
+    device '/dev/xvdi'
+    fstype 'ext4'
+    action [:mount, :enable]
+  end
 end
--- spec/cf-jenkins/aws_spec.rb
+++ spec/cf-jenkins/aws_spec.rb
@@ -1,35 +1,57 @@
 require 'spec_helper'
 
 describe 'cf-jenkins::aws' do
-  subject(:chef_run) { ChefSpec::Runner.new.converge(described_recipe) }
-
-  let(:jenkins_home) { chef_run.node[:jenkins][:server][:home] }
-  let(:aws_access_key) { chef_run.node['aws']['access_key'] }
-  let(:aws_secret_access_key) { chef_run.node['aws']['secret_access_key'] }
-
-  before do
-    stub_command('file -s /dev/xvdi | grep ext4').and_return(formatted?)
+  subject(:chef_run) do
+    ChefSpec::Runner.new do |node|
+      node.set['aws'] = aws_config
+    end.converge(described_recipe)
   end
 
-  let(:formatted?) { true }
+  let(:jenkins_home) { chef_run.node[:jenkins][:server][:home] }
+  let(:aws_config) { {} }
 
-  it { should associate_aws_elastic_ip('jenkins ip').
-                with(aws_access_key: aws_access_key,
-                     aws_secret_access_key: aws_secret_access_key,
-                     ip: chef_run.node['aws']['elastic_ip']) }
+  it { should_not associate_aws_elastic_ip('jenkins ip') }
+  it { should_not attach_aws_ebs_volume('jenkins_ebs_home') }
+  it { should_not run_execute('format drive') }
+  it { should_not create_directory(jenkins_home) }
+  it { should_not enable_mount(jenkins_home) }
 
-  it { should attach_aws_ebs_volume('jenkins_ebs_home').
-                with(aws_access_key: aws_access_key,
-                     aws_secret_access_key: aws_secret_access_key,
-                     device: '/dev/sdi') }
+  context 'when node.aws.access_key and node.aws.secret_access_key are set' do
+    let(:aws_access_key) { chef_run.node['aws']['access_key'] }
+    let(:aws_secret_access_key) { chef_run.node['aws']['secret_access_key'] }
 
-  it { should_not run_execute('format drive').with(command: 'mkfs -t ext4 /dev/xvdi') }
-  it { should create_directory(jenkins_home).with(owner: 'root', group: 'root', mode: 00644) }
-  it { should enable_mount(jenkins_home).with(device: '/dev/xvdi', fstype: 'ext4') }
+    let(:aws_config) do
+      {
+        'access_key' => 'ACCESS_KEY',
+        'secret_access_key' => 'SECRET_ACCESS_KEY',
+        'elastic_ip' => 'ELASTIC_IP',
+      }
+    end
 
-  context 'when the drive is NOT formatted' do
-    let(:formatted?) { false }
+    before do
+      stub_command('file -s /dev/xvdi | grep ext4').and_return(formatted?)
+    end
 
-    it { should run_execute('format drive').with(command: 'mkfs -t ext4 /dev/xvdi') }
+    let(:formatted?) { true }
+
+    it { should associate_aws_elastic_ip('jenkins ip').
+                  with(aws_access_key: aws_access_key,
+                       aws_secret_access_key: aws_secret_access_key,
+                       ip: chef_run.node['aws']['elastic_ip']) }
+
+    it { should attach_aws_ebs_volume('jenkins_ebs_home').
+                  with(aws_access_key: aws_access_key,
+                       aws_secret_access_key: aws_secret_access_key,
+                       device: '/dev/sdi') }
+
+    it { should_not run_execute('format drive').with(command: 'mkfs -t ext4 /dev/xvdi') }
+    it { should create_directory(jenkins_home).with(owner: 'root', group: 'root', mode: 00644) }
+    it { should enable_mount(jenkins_home).with(device: '/dev/xvdi', fstype: 'ext4') }
+
+    context 'when the drive is NOT formatted' do
+      let(:formatted?) { false }
+
+      it { should run_execute('format drive').with(command: 'mkfs -t ext4 /dev/xvdi') }
+    end
   end
 end
