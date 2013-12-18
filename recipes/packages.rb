3fe6bf3ef92e8f2b33b411098715e0d489f43e2f
Andrew Bruce <abruce@pivotallabs.com>
2013-12-18 14:42:34 -0800

    Don't install unnecessary packages
    Signed-off-by: Mark Rushakoff <mrushakoff@pivotallabs.com>

--- recipes/packages.rb
+++ recipes/packages.rb
@@ -5,10 +5,6 @@ package 'vim'
 package 'libxslt-dev'
 package 'libxml2-dev'
 
-# for yetis
-package 'maven'
-package 'zip'
-
 # various libs for bundle package during release creation
 # dea_next:
 package 'libcurl4-openssl-dev'
@@ -20,9 +16,3 @@ package 'libsqlite3-dev'
 
 # for installing bzr go dependencies (for spiff)
 package 'bzr'
-
-# for capybara-webkit
-package 'qt4-dev-tools'
-package 'libqt4-dev'
-package 'libqt4-core'
-package 'libqt4-gui'
--- spec/cf-jenkins/packages_spec.rb
+++ spec/cf-jenkins/packages_spec.rb
@@ -8,16 +8,9 @@ describe 'cf-jenkins::packages' do
   it { should install_package('vim') }
   it { should install_package('libxslt-dev') }
   it { should install_package('libxml2-dev') }
-  it { should install_package('maven') }
-  it { should install_package('zip') }
   it { should install_package('libcurl4-openssl-dev') }
   it { should install_package('libpq-dev') }
   it { should install_package('libmysqlclient-dev') }
   it { should install_package('libsqlite3-dev') }
   it { should install_package('bzr') }
-
-  it { should install_package('qt4-dev-tools') }
-  it { should install_package('libqt4-dev') }
-  it { should install_package('libqt4-core') }
-  it { should install_package('libqt4-gui') }
 end
