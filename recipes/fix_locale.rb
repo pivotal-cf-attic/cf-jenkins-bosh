7e8d5ab4f662004f3ba226230bf752071d5dd687
Andrew Bruce <abruce@pivotallabs.com>
2013-12-20 10:23:00 -0800

    Add fix_locale recipe
    Signed-off-by: Mark Rushakoff <mrushakoff@pivotallabs.com>

---
+++ recipes/fix_locale.rb
@@ -0,0 +1,5 @@
+# Some VMs have issues with defaulting to ASCII encoding;
+# see https://tickets.opscode.com/browse/CHEF-3304
+Encoding.default_external = Encoding::UTF_8
+Encoding.default_internal = Encoding::UTF_8
+
