c377d610523160c995629a9560d44f4511bb76e1
Andrew Bruce <andrew@camelpunch.com>
2013-12-30 09:30:36 -0800

    Fix GVM tests
    Signed-off-by: Mark Rushakoff <mrushakoff@pivotallabs.com>

--- spec/cf-jenkins/recipes/gvm_spec.rb
+++ spec/cf-jenkins/recipes/gvm_spec.rb
@@ -13,7 +13,29 @@ describe 'cf-jenkins::gvm' do
   it { should install_package('gcc') }
 
   it { should create_remote_file(gvm_installer).with(mode: 0755, source: 'https://raw.github.com/moovweb/gvm/master/binscripts/gvm-installer') }
-  it { should run_bash('install GVM').with(code: "#{gvm_installer} master /usr/local/share") }
   it { should run_bash('install go 1.2').with(code: 'source /usr/local/share/gvm/scripts/gvm && gvm install go1.2') }
   it { should create_file('/etc/profile.d/gvm.sh').with(mode: 0755) }
+
+  describe 'installing GVM' do
+    let(:gvm_script) { '/usr/local/share/gvm/scripts/gvm' }
+    context 'when GVM was already installed' do
+      before do
+        exists = File.method(:exists?)
+        File.stub(:exists?) do |thing|
+          thing == gvm_script || exists[thing]
+        end
+      end
+      it { should_not run_bash('install GVM') }
+    end
+
+    context 'when GVM has not been installed yet' do
+      before do
+        exists = File.method(:exists?)
+        File.stub(:exists?) do |thing|
+          exists[thing] if thing != gvm_script
+        end
+      end
+      it { should run_bash('install GVM').with(code: "#{gvm_installer} master /usr/local/share") }
+    end
+  end
 end
