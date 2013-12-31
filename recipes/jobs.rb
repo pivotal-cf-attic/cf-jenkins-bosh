94c4d286897e873ee309b28e192c91baa9efc6db
Matt Royal <mroyal@pivotallabs.com>
2013-12-31 15:34:07 -0800

    Attempt to fix problem with new deploys
    New deploys of jenkins aren't setting the origin of the build repo. This
    is an attempt to fix the problem.

--- recipes/jobs.rb
+++ recipes/jobs.rb
@@ -10,6 +10,14 @@ node['cf_jenkins']['jobs'].each do |job_settings|
     action :create
   end
 
+  build_repo = job_settings[:build_vars].fetch(:build_repo)
+  build_repo_directory = job_settings[:build_vars][:build_repo_directory]
+  cmd_to_fix_build_repo_remote = "git remote add -f origin #{build_repo}"
+  if build_repo_directory
+    cmd_to_fix_build_repo_remote = "(cd #{build_repo_directory}; #{cmd_to_fix_build_repo_remote})"
+  end
+
+  job_settings[:build_vars][:cmd_to_fix_build_repo_remote] = cmd_to_fix_build_repo_remote
   template job_config do
     source job_template_source
     owner node['jenkins']['server']['user']
