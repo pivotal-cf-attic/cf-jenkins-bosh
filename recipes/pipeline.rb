5e4775c27f3aafe9343ab59c1f29c7a1b8aa5da4
Andrew Bruce <andrew@camelpunch.com>
2013-12-31 16:45:55 -0800

    Remove more pipeline-specific recipes
    Signed-off-by: Mark Rushakoff <mrushakoff@pivotallabs.com>

--- recipes/pipeline.rb
+++
@@ -1,8 +0,0 @@
-# Minimal recipes to get Jenkins running
-include_recipe 'cf-jenkins::jenkins_base'
-
-# Able to specify pipelines by supplying Git, etc. info
-include_recipe 'cf-jenkins::pipelines'
-
-# Install only the plugins needed for pipeline steps
-include_recipe 'cf-jenkins::pipeline_jenkins_plugins'
