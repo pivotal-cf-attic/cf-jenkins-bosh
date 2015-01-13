b801321a18ec3e12d60e62125afeacd91d6d451c
Maria Shaldibina <mshaldibina@pivotal.io>
2015-01-12 18:07:57 -0800

    Fix rubies cookbook
    also fix tests
    
    Signed-off-by: Mehran Saliminia <msaliminia@gmail.com>

--- spec/cf-jenkins/recipes/node_spec.rb
+++ spec/cf-jenkins/recipes/node_spec.rb
@@ -6,7 +6,7 @@ describe 'cf-jenkins::node' do
   before { stub_include_recipe_calls }
 
   it { expect(chef_run).to include_recipe('apt') }
-  it { expect(chef_run).to include_recipe('chef_rubies') }
+  it { expect(chef_run).to include_recipe('rubies') }
   it { expect(chef_run).to include_recipe('cf-jenkins::gvm') }
   it { expect(chef_run).to include_recipe('cf-jenkins::packages') }
 end
